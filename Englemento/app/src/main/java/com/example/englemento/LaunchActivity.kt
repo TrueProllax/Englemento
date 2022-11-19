25b08288b64e18a27319e3db006e44669119bb1f
LobanovAntony <ranobeshnik.fantazii@gmail.com>
2022-11-19 10:32:31 +0700

    add launch

---
+++ Englemento/app/src/main/java/com/example/englemento/LaunchActivity.kt
@@ -0,0 +1,19 @@
+package com.example.englemento
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val Intent = Intent(this,MainActivity::class.java)
+            startActivity(Intent)
+        },3000);
+    }
+}
